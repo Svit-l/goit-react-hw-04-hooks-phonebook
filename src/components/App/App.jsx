963496615b7e36c4be34dd9e59f83a1812c3c8f8
Svit-l <lov.svit@gmail.com>
2022-02-04 01:26:15 +0200

    refactor: to hooks adding, removing and filter

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-// import { nanoid } from 'nanoid';
+import { useState, useEffect, useMemo } from 'react';
+import { toast } from 'react-toastify';
 
 import s from './App.module.css';
 
@@ -7,7 +7,7 @@ import ContactForm from '../ContactForm';
 import Filter from '../Filter';
 import ContactList from '../ContactList';
 
-export default function App(oneContact) {
+export default function App() {
   // const { name, number, id } = oneContact;
 
   const [contacts, setContacts] = useState([
@@ -23,19 +23,14 @@ export default function App(oneContact) {
   const [filtered, setFiltered] = useState('');
 
   const addContacts = oneContact => {
-    const { name, number, id } = oneContact;
-
-    console.log(contacts);
-    console.log(name);
-    console.log(oneContact);
-    // const newContacts = setContacts(prevContacts => [
-    //   ...prevContacts,
-    //   oneContact,
-    // ]);
-    // console.log(newContacts);
-    // contacts.some(contact => contact.name === name)
-    //   ? alert(`${name} is already in contacts`)
-    //   : setContacts(() => [...contacts, oneContact]);
+    const { name } = oneContact;
+    // console.log(name);
+    // console.log(oneContact);
+    contacts.some(contact => contact.name === name)
+      ? toast.warn(`${name} is already in contacts`, {
+          theme: 'colored',
+        })
+      : setContacts(prevContacts => [...prevContacts, oneContact]);
   };
 
   // useEffect(() => {
@@ -64,28 +59,36 @@ export default function App(oneContact) {
   //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
   //   }
   // }
-  const deleteContact = contactId => {
-    // console.log(contactId);
-    const result = window.confirm('Are you sure? Contact cannot be restored!');
-    result
-      ? setContacts(prevContacts => {
-          prevContacts.filter(contact => contact.id !== contactId);
-        })
-      : alert('The contact has not been deleted!');
-  };
 
   const changeFilter = e => {
     const { value } = e.currentTarget;
     setFiltered(value);
   };
 
-  const getFilteredContacts = () => {
+  const getFilteredContacts = useMemo(() => {
     const normalizedFilter = filtered.toLowerCase();
+    if (contacts) {
+      return contacts.filter(contact =>
+        contact.name.toLowerCase().includes(normalizedFilter)
+      );
+    }
+  }, [filtered, contacts]);
 
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
+  const deleteContact = contactId => {
+    const result = window.confirm('Are you sure? Contact cannot be restored!');
+    result
+      ? setContacts(prevContacts => {
+          toast.success(`The contact has been deleted`, {
+            theme: 'colored',
+          });
+          return prevContacts.filter(contact => contact.id !== contactId);
+        })
+      : toast.warn(`The contact has not been deleted`, {
+          theme: 'colored',
+        });
   };
+
+  console.log(contacts);
 
   return (
     <div className={s.container}>
@@ -99,7 +102,7 @@ export default function App(oneContact) {
             <h2 className={s.subtitle}>Contacts</h2>
             <Filter value={filtered} event={changeFilter} />
             <ContactList
-              contacts={getFilteredContacts()}
+              contacts={getFilteredContacts}
               onDeleteContact={deleteContact}
             />
           </>
@@ -108,125 +111,3 @@ export default function App(oneContact) {
     </div>
   );
 }
-
-// import React, { Component } from 'react';
-// import { nanoid } from 'nanoid';
-
-// import s from './App.module.css';
-
-// import ContactForm from '../ContactForm';
-// import Filter from '../Filter';
-// import ContactList from '../ContactList';
-
-// class App extends Component {
-//   state = {
-//     contacts: [
-//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//       { id: '2811', name: 'Moore Hensley', number: '555-55-22' },
-//       { id: '3821', name: 'Sharlene Bush', number: '555-55-23' },
-//       { id: '4421', name: 'Nansy Button', number: '455-55-67' },
-//     ],
-//     filter: '',
-//   };
-
-//   addContacts = ({ name, number }) => {
-//     const { contacts } = this.state;
-//     const contact = {
-//       name,
-//       number,
-//       id: nanoid(),
-//     };
-
-//     contacts.some(contact => contact.name === name)
-//       ? alert(`${name} is already in contacts`)
-//       : this.setState(prevState => ({
-//           contacts: [...prevState.contacts, contact],
-//         }));
-//   };
-
-//   deleteContact = contactId => {
-//     const { contacts } = this.state;
-//     const result = window.confirm('Are you sure? Contact cannot be restored!');
-//     result
-//       ? this.setState({
-//           contacts: contacts.filter(contact => contact.id !== contactId),
-//         })
-//       : alert('The contact has not been deleted!');
-//   };
-
-//   changeFilter = e => {
-//     const { value } = e.currentTarget;
-//     this.setState({ filter: value });
-//   };
-
-//   getFilteredContacts = () => {
-//     const { filter, contacts } = this.state;
-//     const normalizedFilter = filter.toLowerCase();
-
-//     return contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(normalizedFilter)
-//     );
-//   };
-
-//   componentDidMount() {
-//     // console.log('Component App didMount');
-//     const contacts = localStorage.getItem('contacts');
-//     // console.log(contacts);
-//     const parseContacts = JSON.parse(contacts);
-//     // console.log(parseContacts);
-//     if (parseContacts) {
-//       this.setState({ contacts: parseContacts });
-//     }
-//   }
-
-//   componentDidUpdate(_, prevState) {
-//     // console.log('App didUpdate');
-//     if (this.state.contacts !== prevState.contacts) {
-//       // console.log('обновилось');
-//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-//     }
-//   }
-
-//   render() {
-//     // console.log('App render');
-//     const { addContacts, changeFilter, deleteContact } = this;
-//     const { contacts, filter } = this.state;
-//     const filteredContacts = this.getFilteredContacts();
-
-//     return (
-//       <div className={s.container}>
-//         <h1 className={s.title}>Phonebook</h1>
-//         <ContactForm onSubmit={addContacts} />
-//         <div className={s.contactList}>
-//           {contacts.length === 0 ? (
-//             <p>Your phone book is empty, enter your first contact!</p>
-//           ) : (
-//             <>
-//               <h2 className={s.subtitle}>Contacts</h2>
-//               <Filter value={filter} event={changeFilter} />
-//               <ContactList
-//                 contacts={filteredContacts}
-//                 onDeleteContact={deleteContact}
-//               />
-//             </>
-//           )}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
-// +++++++++++++++++++++++++++
-
-// handleInputChange = e => {
-//   console.log(e.currentTarget);
-//   console.log(e.currentTarget.name);
-//   console.log(e.currentTarget.value);
-//   this.setState({ name: e.currentTarget.value.trim() });
-//   this.setState({ [e.currentTarget.name]: e.currentTarget.value.trim() });
-// };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import App from './components/App';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ToastContainer
+      position="top-left"
+      autoClose={3000}
+      hideProgressBar
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable={false}
+      pauseOnHover
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
